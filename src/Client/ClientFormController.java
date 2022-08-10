6f0de8e34d16483a9d4d96fe01196d3a3a4c27fb
KawshithaFdo <praveenkawshika456@gmail.com>
2022-08-10 18:59:49 +0530

    Drop Appinitializer and Add LoginForm To Client

--- src/Client/ClientFormController.java
+++ src/Client/ClientFormController.java
@@ -4,17 +4,45 @@ import javafx.event.ActionEvent;
 import javafx.scene.control.TextField;
 import javafx.scene.layout.AnchorPane;
 
-public class ClientFormController {
-    public String username;
-    public AnchorPane ClientContext;
-    public TextField txtMessage;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.PrintWriter;
+import java.net.Socket;
 
+public class ClientFormController {
+    public TextField txtMessage;
+    public AnchorPane ClientContext;
+
+    public String username;
+    Socket socket=null;
 
     public void initialize(){
+        new Thread(()->{
+            try {
+                String record = "";
+                socket=new Socket("localhost",3000);
+
+                while (!(record.equals("exit"))){
+                    InputStreamReader inputStreamReader = new InputStreamReader(socket.getInputStream());
+                    BufferedReader bufferedReader = new BufferedReader(inputStreamReader);
+                    record = bufferedReader.readLine();
+                    System.out.println(record);
+                }
+
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+
+        }).start();
 
     }
 
-    public void btnSendMessage(ActionEvent actionEvent){
+    public void btnSendMessage(ActionEvent actionEvent) throws IOException {
+        PrintWriter printWriter=new PrintWriter(socket.getOutputStream());
+        printWriter.println(txtMessage.getText());
+        printWriter.flush();
+        txtMessage.setText("");
     }
 }
 
--- src/Server/ServerFormController.java
+++ src/Server/ServerFormController.java
@@ -1,9 +1,42 @@
 package Server;
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.PrintWriter;
+import java.net.ServerSocket;
+import java.net.Socket;
+
 public class ServerFormController {
+    Socket accept;
+
+    public void initialize() throws IOException {
+
+        new Thread(()->{
+            try {
+                //Client Message = record
+                String record = "";
+                ServerSocket serverSocket=new ServerSocket(3000);
+                accept=serverSocket.accept();
 
 
-    public void initialize(){
+                while (!(record.equals("exit"))){
+                    InputStreamReader inputStreamReader = new InputStreamReader(accept.getInputStream());
+                    BufferedReader bufferedReader = new BufferedReader(inputStreamReader);
+                    record = bufferedReader.readLine();
+                    System.out.println(record);
 
+                    PrintWriter printWriter=new PrintWriter(accept.getOutputStream());
+                    printWriter.println(record);
+                    printWriter.flush();
+
+                }
+
+
+
+            }catch (Exception e){
+                e.printStackTrace();
+            }
+        }).start();
     }
 }
