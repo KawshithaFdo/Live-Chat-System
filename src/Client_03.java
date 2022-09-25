d88ae813fe516c543f4c823f1136815edfdb7f67
KawshithaFdo <praveenkawshika456@gmail.com>
2022-09-25 23:27:02 +0530

    Add Client_03

---
+++ src/Client_03.java
@@ -0,0 +1,21 @@
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+import java.io.IOException;
+
+public class Client_03 extends Application {
+
+    public static void main(String[] args) {
+        launch(args);
+    }
+
+    @Override
+    public void start(Stage primaryStage) throws IOException {
+        primaryStage.setScene(new Scene(FXMLLoader.load(this.getClass().getResource("Login/LoginForm.fxml"))));
+        primaryStage.setTitle("Client-03");
+        primaryStage.setResizable(false);
+        primaryStage.show();
+    }
+}
